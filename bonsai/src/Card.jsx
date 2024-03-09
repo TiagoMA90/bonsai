93681e769b330368f48f25c714bf7c4e3b8bf06b
TiagoMA90 <Tiago_MA90@hotmail.com>
2024-03-09 22:43:04 +0000

    apply styling with css

---
+++ bonsai/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return(
+        <div className="card">
+            <img className="image" src="https://bonsai.ch/cdn/shop/products/carmona-fukientee-d15-7000_720x.jpg?v=1676737685" alt="Serissa Foedita"></img>
+            <h2 className="title">serissa foetida</h2>
+            <p className="text">The Serissa is also called snow rose, tree of thousand stars or Japanese boxthorn. It is a subtropical small shrub with tiny white flowers that appear from spring through summer. </p>
+        </div>
+    );
+}
+
+export default Card;
